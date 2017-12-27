ad63a2585dc5497daaaa5dd968fe26880c3c448f
jinyalin <jinyalin@baidu.com>
2017-12-27 20:58:22 +0800

    bug fix

--- app/src/main/java/com/yalin/dialogactivity/demo/MainActivity.java
+++ app/src/main/java/com/yalin/dialogactivity/demo/MainActivity.java
@@ -4,61 +4,69 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
 import com.yalin.dialogactivity.demo.ArithmeticFragment.ArithmeticResultCallback;
 import com.yalin.dialogactivity.demo.TestFragment1.Callback1;
 import com.yalin.dialogactivity.demo.TestFragment2.Callback2;
 import com.yalin.dialogactivity.demo.dialog.DialogBuilder;
 
 public class MainActivity extends AppCompatActivity implements Callback1, Callback2,
-    ArithmeticResultCallback {
+        ArithmeticResultCallback {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-  public void showDialog1(View view) {
-    DialogBuilder.from(TestFragment1.class)
-        .on(this)
-        .callback(this)
-        .show();
-  }
+    public void showDialog1(View view) {
+        DialogBuilder.from(TestFragment1.class)
+                .on(this)
+                .callback(this)
+                .show();
+    }
 
-  public void showDialog2(View view) {
-    DialogBuilder.from(TestFragment2.class)
-        .on(this)
-        .callback(this)
-        .show();
-  }
+    public void showDialog2(View view) {
+        DialogBuilder.from(TestFragment2.class)
+                .on(this)
+                .callback(this)
+                .show();
+    }
 
-  public void doArithmetic(View view) {
-    Bundle args = ArithmeticFragment.createArgsBundle(100, 88);
-    DialogBuilder.from(ArithmeticFragment.class)
-        .on(this.getApplicationContext())
-        .callback(this)
-        .args(args)
-        .show();
-  }
+    public void doArithmetic(View view) {
+        Bundle args = ArithmeticFragment.createArgsBundle(100, 88);
+        DialogBuilder.from(ArithmeticFragment.class)
+                .on(this.getApplicationContext())
+                .callback(this)
+                .args(args)
+                .show();
+    }
 
-  @Override
-  public void onCallback1() {
-    Toast.makeText(this, "Callback1", Toast.LENGTH_SHORT).show();
-  }
+    public void test(View view) {
+        showDialog1(view);
+        showDialog2(view);
+    }
 
-  @Override
-  public void onCallback2(String content) {
-    Toast.makeText(this, "Callback2 " + content, Toast.LENGTH_SHORT).show();
-  }
+    @Override
+    public void onCallback1() {
+        Toast.makeText(this, "Callback1", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCallback2(String content) {
+        Toast.makeText(this, "Callback2 " + content, Toast.LENGTH_SHORT).show();
+    }
 
 
-  @Override
-  public void onPlus(int result) {
-    Toast.makeText(this, "onPlus result " + result, Toast.LENGTH_SHORT).show();
-  }
+    @Override
+    public void onPlus(int result) {
+        Toast.makeText(this, "onPlus result " + result, Toast.LENGTH_SHORT).show();
+    }
 
-  @Override
-  public void onSub(int result) {
-    Toast.makeText(this, "onSub result " + result, Toast.LENGTH_SHORT).show();
-  }
+    @Override
+    public void onSub(int result) {
+        Toast.makeText(this, "onSub result " + result, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
--- app/src/main/java/com/yalin/dialogactivity/demo/dialog/DialogBuilder.java
+++ app/src/main/java/com/yalin/dialogactivity/demo/dialog/DialogBuilder.java
@@ -13,52 +13,52 @@ import android.os.Bundle;
 
 public class DialogBuilder<T extends DialogContentFragment<C>, C> {
 
-  private Context mContext;
-  private Class<T> mFragmentClass;
-  private C mCallback;
-  private Bundle mArgs;
+    private Context mContext;
+    private Class<T> mFragmentClass;
+    private C mCallback;
+    private Bundle mArgs;
 
-  public static <T extends DialogContentFragment<C>, C> DialogBuilder<T, C> from(
-      Class<T> fragmentClass) {
-    return new DialogBuilder<>(fragmentClass);
-  }
-
-  private DialogBuilder(Class<T> fragmentClass) {
-    mFragmentClass = fragmentClass;
-  }
-
-  public DialogBuilder<T, C> on(Context context) {
-    mContext = context;
-    return this;
-  }
-
-  public DialogBuilder<T, C> callback(C callback) {
-    mCallback = callback;
-    return this;
-  }
-
-  public DialogBuilder<T, C> args(Bundle args) {
-    mArgs = args;
-    return this;
-  }
-
-  public void show() {
-    Preconditions.checkNotNull(mContext);
-    Preconditions.checkNotNull(mFragmentClass);
-
-    int callBackId = CallbackPool.INVALID_CALLBACK_ID;
-    if (mCallback != null) {
-      callBackId = CallbackPool.getInstance().put(mCallback);
+    public static <T extends DialogContentFragment<C>, C> DialogBuilder<T, C> from(
+            Class<T> fragmentClass) {
+        return new DialogBuilder<>(fragmentClass);
     }
-    Intent intent = new Intent(mContext, DialogActivity.class);
-    intent.putExtra(DialogActivity.CALLBACK_ID, callBackId);
-    intent.putExtra(DialogActivity.FRAGMENT_CLASS, mFragmentClass);
-    if (mArgs != null) {
-      intent.putExtra(DialogActivity.ARGS, mArgs);
+
+    private DialogBuilder(Class<T> fragmentClass) {
+        mFragmentClass = fragmentClass;
     }
-    if (!(mContext instanceof Activity)) {
-      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+    public DialogBuilder<T, C> on(Context context) {
+        mContext = context;
+        return this;
     }
-    mContext.startActivity(intent);
-  }
+
+    public DialogBuilder<T, C> callback(C callback) {
+        mCallback = callback;
+        return this;
+    }
+
+    public DialogBuilder<T, C> args(Bundle args) {
+        mArgs = args;
+        return this;
+    }
+
+    public void show() {
+        Preconditions.checkNotNull(mContext);
+        Preconditions.checkNotNull(mFragmentClass);
+
+        int callBackId = CallbackPool.INVALID_CALLBACK_ID;
+        if (mCallback != null) {
+            callBackId = CallbackPool.getInstance().put(this, mCallback);
+        }
+        Intent intent = new Intent(mContext, DialogActivity.class);
+        intent.putExtra(DialogActivity.CALLBACK_ID, callBackId);
+        intent.putExtra(DialogActivity.FRAGMENT_CLASS, mFragmentClass);
+        if (mArgs != null) {
+            intent.putExtra(DialogActivity.ARGS, mArgs);
+        }
+        if (!(mContext instanceof Activity)) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
+        mContext.startActivity(intent);
+    }
 }
